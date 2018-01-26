f77ce4766ffd2dda8fb5ab7f4176af7d661142af
Dan Cohen <dan.cohen@codurance.com>
2018-01-26 15:53:58 +0000

    built a bot that has no idea how to play tetris

--- engine.py
+++ engine.py
@@ -4,72 +4,94 @@
 from random import randrange as rand
 
 tetris_shapes = [
-	[[1, 1, 1],
-	 [0, 1, 0]],
-	
-	[[0, 2, 2],
-	 [2, 2, 0]],
-	
-	[[3, 3, 0],
-	 [0, 3, 3]],
-	
-	[[4, 0, 0],
-	 [4, 4, 4]],
-	
-	[[0, 0, 5],
-	 [5, 5, 5]],
-	
-	[[6, 6, 6, 6]],
-	
-	[[7, 7],
-	 [7, 7]]
+    [[1, 1, 1],
+     [0, 1, 0]],
+    
+    [[0, 1, 1],
+     [1, 1, 0]],
+    
+    [[1, 1, 0],
+     [0, 1, 1]],
+    
+    [[1, 0, 0],
+     [1, 1, 1]],
+    
+    [[0, 0, 1],
+     [1, 1, 1]],
+    
+    [[1, 1, 1, 1]],
+    
+    [[1, 1],
+     [1, 1]]
 ]
 
 def get_new_piece():
     return tetris_shapes[rand(len(tetris_shapes))]
 
 def new_game(width, height):
-	board = [ [ 0 for x in range(width) ]
-			for y in range(height) ]
-	board += [[ 1 for x in range(width)]]
-	return board, get_new_piece()
+    board = [ [ 0 for x in range(width) ]
+            for y in range(height) ]
+    board += [[ 1 for x in range(width)]]
+    return board, get_new_piece()
 
 
 def check_collision(board, shape, offset):
-	off_x, off_y = offset
-	for cy, row in enumerate(shape):
-		for cx, cell in enumerate(row):
-			try:
-				if cell and board[ cy + off_y ][ cx + off_x ]:
-					return True
-			except IndexError:
-				return True
-	return False
+    position_x, position_y = offset
+    for cy, row in enumerate(shape):
+        for cx, cell in enumerate(row):
+            try:
+                if cell and board[ cy + position_y ][ cx + position_x ]:
+                    return True
+            except IndexError:
+                return True
+    return False
 
 def remove_row(board, row):
-	cols = len(board[0])
-	del board[row]
-	return [[0 for i in range(cols)]] + board
+    cols = len(board[0])
+    del board[row]
+    return [[0 for i in range(cols)]] + board
 
 
-def join_matrices(mat1, mat2, mat2_off): 
-	off_x, off_y = mat2_off 
-	for cy, row in enumerate(mat2): 
-		for cx, val in enumerate(row): 
-			mat1[cy+off_y-1	][cx+off_x] += val 
-	return mat1
+def join_matrices(game_board, piece, position): 
+    position_x, position_y = position 
+    for y, row in enumerate(piece): 
+        for x, val in enumerate(row): 
+            game_board[y+position_y-1][x+position_x] += val 
+    return game_board
 
 def remove_rows(board):
-	cleared_rows = 0
-	while True:
-		for i, row in enumerate(board[:-1]):
-			if 0 not in row:
-				board = remove_row(
-				  board, i)
-				cleared_rows += 1
-				break
-		else:
-			return cleared_rows, board
-def play(move, board):
+    cleared_rows = 0
+    while True:
+        for i, row in enumerate(board[:-1]):
+            if 0 not in row:
+                board = remove_row(
+                  board, i)
+                cleared_rows += 1
+                break
+        else:
+            return cleared_rows, board
+
+def play(move, board, piece):
+    board = join_matrices(board, piece, move) 
     return board, get_new_piece()
 
+def move_is_legal(game_board, piece, position):
+    position_x, position_y = position 
+    is_resting = False
+    for y, row in enumerate(piece): 
+        for x, val in enumerate(row): 
+            combined_y = y + position_y - 1
+            combined_x = x + position_x
+            print("game_board.x = ", len(game_board[0]))
+            print("combined_x = ", combined_x)
+            print("game_board.y = ", len(game_board))
+            print("combined_y = ", combined_y)
+            if(len(game_board) - 1 <= combined_y):
+                return False
+            if(len(game_board[0]) - 1 < combined_x):
+                return False
+            if(game_board[combined_y][combined_x] + val > 1):
+                return False
+            if(val == 1 and game_board[combined_y + 1][combined_x] == 1):
+                is_resting = True
+    return is_resting
--- player.py
+++ player.py
@@ -1,14 +1,16 @@
 import engine
+from random import randint
 # scaffold for what it should look like
 board_width = 10
 board_height = 20
+line = "=============================="
 
-def choose_move(game):
+def choose_move(game, piece):
     search_tree = []
-    print(game)
     for x, row in enumerate(game):
         for y, item in enumerate(row):
-            search_tree.append((x, y))
+            if(engine.move_is_legal(game, piece, (y, x))):
+                search_tree.append((y, x))
     # for position on board:
     #    if engine.is_legal_move(board, piece, position):
     #        search_tree.add(position)
@@ -20,22 +22,45 @@ def choose_move(game):
     #   if best_value < value:
     #       best_value = value
     #       best_move = move
-    best_move = search_tree[5]
+    print("search_tree = ", search_tree)
+    if(len(search_tree) == 0):
+        return None, None
+    best_move = search_tree[randint(0, len(search_tree) - 1)]
     value = 15
-    return (best_move, value)
+    return best_move, value
 
 # def evaluate(board):
 #   cnn_input = board.flatten
 #   cnn.evaluate(cnn_input)
 
 def main():
+    iteration = 1
     while(True):
-       game, piece = engine.new_game(board_width, board_height)
-       game_over = False
-       while(not game_over):
-           move, value = choose_move(game)
-    #       historical_evaluations.push((move, value))
-           game, piece = engine.play(move, game)
-    #   cnn.train(game.score, historical_evaluations)
+        print("Iteration ", iteration)
+        move_number = 1
+        game, piece = engine.new_game(board_width, board_height)
+        game_over = False
+        while(not game_over):
+            print("MOVE NUMBER ", move_number)
+            move, value = choose_move(game, piece)
+            if(move == None):
+                game_over = True
+                continue
+            # historical_evaluations.push((move, value))
+            print("move = ", move)
+            print("game = \n", game_to_string(game))
+            print("piece = ", piece)
+            game, piece = engine.play(move, game, piece)
+            input("hit enter for next move...")
+            move_number += 1
+        # cnn.train(game.score, historical_evaluations)
+        iteration += 1
+
+def game_to_string(game):
+    result = line + '\n'
+    for row in game:
+        result += str(row).replace('0', '  ').replace(',', ' ').replace('1', '<>') + '\n'
+    result += line
+    return result
 
 main() 
