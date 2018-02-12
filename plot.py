a79c51e958c79260bfca7701175ce6a44f097b83
Samantha Cohen <samanthacohen294@gmail.com>
2018-02-12 01:05:58 +0000

    improve plotting

--- plot.py
+++ plot.py
@@ -1,18 +1,42 @@
 import matplotlib.pyplot as pyplot
 import re
 
-file = open("out.txt")
+def main():
+    file = open("out.txt")
+    lines = str.join("", file.readlines())
 
-errors = []
-lines = str.join("", file.readlines())
-compiled = re.compile("error = (\d+.?\d*)")
-errors = compiled.findall(lines)
+    moving_average_window = 100
 
-compiled = re.compile("Actual fitness: (\d+)")
-final_scores = compiled.findall(lines)
+    compiled = re.compile("error = (\d+.?\d*)")
+    errors = list(map(float, compiled.findall(lines)))
+    errors_moving_average = get_moving_average(errors, moving_average_window)
 
-pyplot.subplot(211)
-pyplot.semilogy(errors)
-pyplot.subplot(212)
-pyplot.semilogy(final_scores)
-pyplot.show()
+    compiled = re.compile("Actual fitness: (\d+)")
+    final_scores = list(map(float, compiled.findall(lines)))
+    final_scores_moving_average = get_moving_average(final_scores, moving_average_window)
+
+    pyplot.subplot(211)
+    pyplot.plot(errors)
+    pyplot.plot(errors_moving_average)
+    pyplot.xlabel("iteration")
+    pyplot.ylabel("error")
+
+    pyplot.subplot(212)
+    pyplot.plot(final_scores)
+    pyplot.plot(final_scores_moving_average)
+    pyplot.xlabel("iteration")
+    pyplot.ylabel("fitness")
+    pyplot.show()
+
+def get_moving_average(data, window):
+    moving_averages = []
+    for index, datum in enumerate(data):
+        if index >= window - 1:
+            window_data = []
+            for look_back in range(window):
+                window_data.append(data[index - look_back])
+            average = sum(window_data) / window
+            moving_averages.append(average)
+    return moving_averages
+
+main()
