5ad99de9f8da493c87a0c36de1dfd5bfe6d761c7
Dan Cohen <dan.cohen@euromoneyplc.com>
2018-04-01 20:54:29 +0100

    evaluating correctly

--- evaluator.py
+++ evaluator.py
@@ -12,6 +12,9 @@ class Evaluator(object):
     def __init__(self, training_rate):
         self.model = Sequential()
         self.model.add(Dense(units=64, activation='relu', input_dim=200))
+        self.model.add(Dense(units=64, activation='relu', input_dim=200))
+        self.model.add(Dense(units=64, activation='relu', input_dim=200))
+        self.model.add(Dense(units=64, activation='relu', input_dim=200))
         self.model.add(Dense(units=64, activation='relu'))
         self.model.add(Dense(units=1, activation='linear'))
         self.model.compile(loss='mean_squared_error',
@@ -26,40 +29,41 @@ class Evaluator(object):
         return(prediction)
 
     def train(self, final_fitness):
-        self.episodes.append((self.current_episode, final_fitness))
-        while len(self.episodes) > 1000:
-            self.episodes = self.episodes[1:]
+        if(final_fitness > 100):
+            self.episodes.append((self.current_episode, final_fitness))
+            while len(self.episodes) > 1000:
+                self.episodes = self.episodes[1:]
 
-        x_batch = []
-        y_batch = []
-        for episode in self.episodes:
-            episode_fitness = episode[1]
-            for evaluation in episode[0]:
-                value = evaluation[1]
-                moves_so_far = evaluation[2]
-                score_so_far = evaluation[3]
-                fitness_so_far = self.calculate_fitness(score_so_far, moves_so_far)
-                effective_fitness = episode_fitness - fitness_so_far
-                activations = get_inputs_from_board(evaluation[0])
-                x_batch.append(activations)
-                y_batch.append(effective_fitness)
-        log.debug("Doing training against " + str(len(x_batch)) + " items.")
-        x_batch = np.array(x_batch)
-        y_batch = np.array(y_batch)
-        x_batch, y_batch = self.unison_shuffled_copies(x_batch, y_batch)
-        train_length = int(len(x_batch) * 0.8)
-        x_train = x_batch[:train_length]
-        y_train = y_batch[:train_length]
-        x_test = x_batch[train_length:]
-        y_test = y_batch[train_length:]
-        history = self.model.fit(x_train, y_train, epochs=2)
-        log.out("initial error = ", history.history["loss"][0])
-        log.out("final error = ", history.history["loss"][-1])
-        network_evaluation = self.model.evaluate(x_test, y_test)
-        log.out("evaluation error = ", network_evaluation)
-        weights = self.model.get_weights()
-        log.weights(weights)
-        self.current_episode = []
+            x_batch = []
+            y_batch = []
+            for episode in self.episodes:
+                episode_fitness = episode[1]
+                for evaluation in episode[0]:
+                    value = evaluation[1]
+                    moves_so_far = evaluation[2]
+                    score_so_far = evaluation[3]
+                    fitness_so_far = self.calculate_fitness(score_so_far, moves_so_far)
+                    effective_fitness = episode_fitness - fitness_so_far
+                    activations = get_inputs_from_board(evaluation[0])
+                    x_batch.append(activations)
+                    y_batch.append(effective_fitness)
+            log.debug("Doing training against " + str(len(x_batch)) + " items.")
+            x_batch = np.array(x_batch)
+            y_batch = np.array(y_batch)
+            x_batch, y_batch = self.unison_shuffled_copies(x_batch, y_batch)
+            train_length = int(len(x_batch) * 0.8)
+            x_train = x_batch[:train_length]
+            y_train = y_batch[:train_length]
+            x_test = x_batch[train_length:]
+            y_test = y_batch[train_length:]
+            history = self.model.fit(x_train, y_train, epochs=2)
+            log.out("initial error = ", history.history["loss"][0])
+            log.out("final error = ", history.history["loss"][-1])
+            network_evaluation = self.model.evaluate(x_test, y_test)
+            log.out("evaluation error = ", network_evaluation)
+            weights = self.model.get_weights()
+            log.weights(weights)
+            self.current_episode = []
 
     def calculate_fitness(self, score, moves):
         return score * 100 + moves
